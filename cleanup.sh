6557b6b92679cff477c33d309fe01c6e0e800f76
Phong Nguyen Le <ple@elasticbox.com>
2014-11-12 18:40:56 -0800

    Added load test script, updated cleanup script

--- cleanup.sh
+++ cleanup.sh
@@ -1,33 +1,44 @@
 #!/bin/bash
 
-EBX_ADDRESS=$1
-TOKEN=$2
+if [[ $# -lt 1 ]]
+then
+	echo "Usage: cleanup.sh EBX_ADDRESS EBX_WORKSPACE TAG"
+	exit 1
+fi
+
+EBX_ADDRESS=${1}
+EBX_WORKSPACE=${2}
+TAG=${3}
 
 function delete_boxes() {
 	BOX_NAME_PART=${1}
-	BOX_IDS=$(eb boxes list --token ${TOKEN} --address ${EBX_ADDRESS} --no-keychain -i tphongio | grep ${BOX_NAME_PART} | awk '{ print $1 }')
+	BOX_IDS=$(eb boxes list --token ${EBX_TOKEN} --address ${EBX_ADDRESS} --no-keychain -i ${EBX_WORKSPACE} -t ${TAG} | awk '{ print $1 }')
 	for BOX_ID in ${BOX_IDS}
 	do
 		BOX_URL=${EBX_ADDRESS}/services/boxes/${BOX_ID}
 		echo Deleting box ${BOX_URL}	
-		curl -k -X DELETE -H "ElasticBox-Token: ${TOKEN}" ${BOX_URL}	
+		curl -k# -X DELETE -H "ElasticBox-Token: ${EBX_TOKEN}" ${BOX_URL}	
 	done
 }
 
 sudo pip install ebcli --upgrade
 
-PROVIDER_IDS=$(curl -k -H "ElasticBox-Token: ${TOKEN}" ${EBX_ADDRESS}/services/workspaces/tphongio/providers | python -m json.tool | grep /services/providers | sed -e 's|/services/providers/||g' -e 's/"//g' | awk '{ print $2 }')
+cd $(dirname $0)
+REPOSITORY_FOLDER=$(pwd)
+source ${REPOSITORY_FOLDER}/common.sh
+
+EBX_TOKEN=$(ebx_token test_admin@elasticbox.com elasticbox)
+
+PROVIDER_IDS=$(curl -k# -H "ElasticBox-Token: ${EBX_TOKEN}" ${EBX_ADDRESS}/services/workspaces/${EBX_WORKSPACE}/providers | python -m json.tool | grep /services/providers | sed -e 's|/services/providers/||g' -e 's/"//g' | awk '{ print $2 }')
 for PROVIDER_ID in ${PROVIDER_IDS}
 do
-	if [ ${PROVIDER_ID} != '345b6945-8834-4901-9090-d3e64535fd12' && ${PROVIDER_ID} != '198c6ae0-17f1-4b82-ba78-03c6b6c52e4f' ]
+	if [ -z $(echo ${EXCLUDED_PROVIDERS} | grep ${PROVIDER_ID}) ]
 	then
 		PROVIDER_URL=${EBX_ADDRESS}/services/providers/${PROVIDER_ID}
 		echo Deleting provider ${PROVIDER_URL}
-		curl -k -X DELETE -H "ElasticBox-Token: ${TOKEN}" ${PROVIDER_URL}
+		curl -k# -X DELETE -H "ElasticBox-Token: ${EBX_TOKEN}" ${PROVIDER_URL}
+		echo
 	fi
 done
 
-delete_boxes test-deeply-nested-box-
-delete_boxes test-nested-box-
-delete_boxes test-linux-box-
-delete_boxes test-binding-box-
+delete_boxes
---
+++ load-test.sh
@@ -0,0 +1,61 @@
+#/bin/bash
+USAGE="Usage : load-test.sh -j JENKINS_URL
+
+Example:
+    load-test.sh -j http://localhost:8080/jenkins
+
+Options:
+    -j Jenkins server URL
+    -n Number of builds per jobs
+    -h Display this message
+"
+
+function help() {
+    echo "${USAGE}"
+
+    if [[ ${1} ]]
+    then
+        echo ${1}
+    fi 
+}
+
+# Handle options
+while getopts ":j:n:h" ARGUMENT
+do
+    case ${ARGUMENT} in
+
+        j )  JENKINS_URL=$OPTARG;;
+        n )  BUILDS=$(($OPTARG));;
+        h )  help; exit 0;;
+        : )  help "Missing option argument for -$OPTARG"; exit 1;;
+        ? )  help "Option does not exist: $OPTARG"; exit 1;;
+                 
+    esac
+done
+
+if [[ -z "${JENKINS_URL}" ]]
+then
+    help "Jenkins server URL must be specified"
+    exit 1
+fi
+
+if [[ ${BUILDS} -lt 1 ]]
+then
+    BUILDS=1
+fi
+
+function jenkins() {
+    java -jar jenkins-cli.jar -s ${JENKINS_URL} "$@"
+}
+
+echo "Downloading jenkins-cli.jar"
+curl ${JENKINS_URL}/jnlpJars/jenkins-cli.jar -o jenkins-cli.jar
+for JOB in $(jenkins list-jobs enabled)
+do
+    echo "Scheduling ${BUILDS} builds of job ${JOB}"
+    for (( i = 0; i < ${BUILDS}; i++ ))
+    do
+        jenkins build $JOB -p hash=$i
+    done
+done
+
